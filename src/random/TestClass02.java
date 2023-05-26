38f836381825b7f0ca9d905d66150aa6a5f4304d
testuser <testuser@test.com>
2023-05-26 16:55:54 +0900

    nextline

---
+++ src/random/TestClass02.java
@@ -0,0 +1,16 @@
+package random;
+
+import java.util.Scanner;
+
+public class TestClass02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n1, n2;
+		System.out.println("문자열 입력1");
+		n1 = sc.nextLine();
+		System.out.println("문자열 입력2");
+		n2 = sc.nextLine();
+		System.out.println(n1);
+		System.out.println(n2);
+	}
+}
